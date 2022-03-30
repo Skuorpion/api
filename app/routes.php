12374f82cf2c8bde2d01b6e0858bdeca3a84422f
Valentin FRANÇOIS <valentin.francois.60@gmail.com>
2022-03-30 15:58:14 +0200

    Exercice 6: création d'un endpoint pour récupérer un dossier

--- app/routes.php
+++ app/routes.php
@@ -9,6 +9,8 @@ use Ramsey\Uuid\Uuid;
 
 return function (App $app) {
 
+    /*********** Ecritures ***********/
+
     $app->get('/dossiers/{uuid}/ecritures', function (Request $request, Response $response) {
         $dossier = $request->getAttribute('uuid');
 
@@ -162,6 +164,42 @@ return function (App $app) {
             // print out the result as json format
             $newResponse = $response->withStatus(204);
             return $newResponse;
+        } catch (PDOException $e) {
+            // show error message as Json format
+            $response->getBody()->write('{"error": {"msg": ' . $e->getMessage() . '}');
+            return $response;
+        }
+    });
+
+
+
+
+
+    /*********** Dossiers ***********/
+
+    $app->get('/dossiers/{uuid}', function (Request $request, Response $response) {
+        $dossier = $request->getAttribute('uuid');
+
+        $sql = 'SELECT name
+                FROM dossiers
+                WHERE uuid = :dossier';
+
+        try {
+            // Get DB Object
+            $db = new db();
+            // connect to DB
+            $db = $db->connect();
+
+            // query
+            $stmt = $db->prepare( $sql );
+            $stmt->bindParam(':dossier', $dossier);
+            $stmt->execute();
+            $arts = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
+
+            // print out the result as json format
+            $response->getBody()->write(json_encode($arts));
+            return $response;
         } catch (PDOException $e) {
             // show error message as Json format
             $response->getBody()->write('{"error": {"msg": ' . $e->getMessage() . '}');
