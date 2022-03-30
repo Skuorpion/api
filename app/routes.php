a54ec71f054b111f006fec9062bef7634a14c5a1
Valentin FRANÇOIS <valentin.francois.60@gmail.com>
2022-03-30 15:27:34 +0200

    Exercice 4: création d'un endpoint pour modifier une ecriture

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,7 @@ use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 use Ramsey\Uuid\Uuid;
 
 return function (App $app) {
+
     $app->get('/dossiers/{uuid}/ecritures', function (Request $request, Response $response) {
         $dossier = $request->getAttribute('uuid');
 
@@ -84,6 +85,52 @@ return function (App $app) {
 
             $newResponse = $response->withStatus(201);
             $newResponse->getBody()->write('{"' . $uuid . '": "uuid qui vient d\'être généré"}');
+            return $newResponse;
+        } catch (PDOException $e) {
+            // show error message as Json format
+            $response->getBody()->write('{"error": {"msg": ' . $e->getMessage() . '}');
+            return $response;
+        }
+    });
+
+    $app->put('/dossiers/{uuidDossier}/ecritures/{uuidEcriture}', function (Request $request, Response $response) {
+        $dossier = $request->getAttribute('uuidDossier');
+        $ecriture = $request->getAttribute('uuidEcriture');
+        $values = json_decode($request->getBody()->getContents());
+
+        $tempDate = explode('-', $values->date);
+        if ($values->amount < 0 || !checkdate(intval($tempDate[1]), intval($tempDate[2]), intval($tempDate[0])) ) {
+            $newResponse = $response->withStatus(400 );
+            return $newResponse;
+        }
+
+        $sql = 'UPDATE ecritures SET 
+                    label = :label,
+                    date = :date,
+                    type = :type,
+                    amount = :amount,
+                    dossier_uuid = :dossier_uuid
+                WHERE uuid = :uuid';
+
+        try {
+            // Get DB Object
+            $db = new db();
+
+            // connect to DB
+            $db = $db->connect();
+
+            // query
+            $stmt = $db->prepare( $sql );
+            $stmt->bindParam(':uuid', $ecriture);
+            $stmt->bindParam(':label', $values->label);
+            $stmt->bindParam(':date', $values->date);
+            $stmt->bindParam(':type', $values->type);
+            $stmt->bindParam(':amount', $values->amount);
+            $stmt->bindParam(':dossier_uuid', $dossier);
+            $stmt->execute();
+            $db = null;
+
+            $newResponse = $response->withStatus(204);
             return $newResponse;
         } catch (PDOException $e) {
             // show error message as Json format
