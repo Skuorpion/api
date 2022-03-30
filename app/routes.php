a52444697006e3d3980256152393278f46641e84
Valentin FRANÇOIS <valentin.francois.60@gmail.com>
2022-03-30 15:13:24 +0200

    Exercice 3: création d'un endpoint pour l'ajout d'une ecriture

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Ramsey\Uuid\Uuid;
 
 return function (App $app) {
     $app->get('/dossiers/{uuid}/ecritures', function (Request $request, Response $response) {
@@ -30,6 +31,60 @@ return function (App $app) {
             // print out the result as json format
             $response->getBody()->write(json_encode($arts));
             return $response;
+        } catch (PDOException $e) {
+            // show error message as Json format
+            $response->getBody()->write('{"error": {"msg": ' . $e->getMessage() . '}');
+            return $response;
+        }
+    });
+
+    $app->post('/dossiers/{uuid}/ecritures', function (Request $request, Response $response) {
+        $dossier = $request->getAttribute('uuid');
+        $values = json_decode($request->getBody()->getContents());
+        $uuid = Uuid::uuid4();
+
+        $tempDate = explode('-', $values->date);
+        if ($values->amount < 0 || !checkdate(intval($tempDate[1]), intval($tempDate[2]), intval($tempDate[0])) ) {
+            $newResponse = $response->withStatus(400 );
+            return $newResponse;
+        }
+
+        $sql = 'INSERT INTO ecritures (
+                        uuid,
+                        label,
+                        date,
+                        type,
+                        amount,
+                        dossier_uuid
+                    ) VALUES (
+                        :uuid,
+                        :label,
+                        :date,
+                        :type,
+                        :amount,
+                        :dossier_uuid
+                    )';
+
+        try {
+            // Get DB Object
+            $db = new db();
+            // connect to DB
+            $db = $db->connect();
+
+            // query
+            $stmt = $db->prepare( $sql );
+            $stmt->bindParam(':uuid', $uuid);
+            $stmt->bindParam(':label', $values->label);
+            $stmt->bindParam(':date', $values->date);
+            $stmt->bindParam(':type', $values->type);
+            $stmt->bindParam(':amount', $values->amount);
+            $stmt->bindParam(':dossier_uuid', $dossier);
+            $stmt->execute();
+            $db = null;
+
+            $newResponse = $response->withStatus(201);
+            $newResponse->getBody()->write('{"' . $uuid . '": "uuid qui vient d\'être généré"}');
+            return $newResponse;
         } catch (PDOException $e) {
             // show error message as Json format
             $response->getBody()->write('{"error": {"msg": ' . $e->getMessage() . '}');
