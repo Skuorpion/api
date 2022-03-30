2a2873f52c193add0905be6d525cdc931deb3f8c
Valentin FRANÇOIS <valentin.francois.60@gmail.com>
2022-03-30 16:33:54 +0200

    Exercice 8: création d'un endpoint pour modifier un dossier

--- app/routes.php
+++ app/routes.php
@@ -28,11 +28,11 @@ return function (App $app) {
             $stmt = $db->prepare( $sql );
             $stmt->bindParam(':dossier', $dossier);
             $stmt->execute();
-            $arts = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $ecritures = $stmt->fetchAll(PDO::FETCH_OBJ);
             $db = null;
 
             // print out the result as json format
-            $response->getBody()->write(json_encode($arts));
+            $response->getBody()->write(json_encode($ecritures));
             return $response;
         } catch (PDOException $e) {
             // show error message as Json format
@@ -194,11 +194,11 @@ return function (App $app) {
             $stmt = $db->prepare( $sql );
             $stmt->bindParam(':dossier', $dossier);
             $stmt->execute();
-            $arts = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $dossiers = $stmt->fetchAll(PDO::FETCH_OBJ);
             $db = null;
 
             // print out the result as json format
-            $response->getBody()->write(json_encode($arts));
+            $response->getBody()->write(json_encode($dossiers));
             return $response;
         } catch (PDOException $e) {
             // show error message as Json format
@@ -245,6 +245,44 @@ return function (App $app) {
 
             $newResponse = $response->withStatus(201);
             $newResponse->getBody()->write('{"' . $uuid . '": "uuid qui vient d\'être généré"}');
+            return $newResponse;
+        } catch (PDOException $e) {
+            // show error message as Json format
+            $response->getBody()->write('{"error": {"msg": ' . $e->getMessage() . '}');
+            return $response;
+        }
+    });
+
+    $app->put('/dossiers/{uuid}', function (Request $request, Response $response) {
+        $dossier = $request->getAttribute('uuid');
+        $values = json_decode($request->getBody()->getContents());
+
+        if (!property_exists($values, 'login') || !property_exists($values, 'password')) {
+            $newResponse = $response->withStatus(400 );
+            return $newResponse;
+        }
+
+        $sql = 'UPDATE dossiers SET 
+                    password = :password,
+                    name = :name
+                WHERE uuid = :uuid';
+
+        try {
+            // Get DB Object
+            $db = new db();
+
+            // connect to DB
+            $db = $db->connect();
+
+            // query
+            $stmt = $db->prepare( $sql );
+            $stmt->bindParam(':uuid', $dossier);
+            $stmt->bindParam(':password', $values->password);
+            $stmt->bindParam(':name', $values->name);
+            $stmt->execute();
+            $db = null;
+
+            $newResponse = $response->withStatus(204);
             return $newResponse;
         } catch (PDOException $e) {
             // show error message as Json format
